0f08bbeba89e77707a95949b84bbe0fc0ad4d540
Jorge <dam0ne@hemwick>
2020-06-03 16:54:19 +0200

    added owner contact info properties

--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Owner.java
@@ -4,6 +4,35 @@ import java.util.Set;
 
 public class Owner extends Person {
 
+    private String address;
+    private String city;
+    private String telephone;
+    private Set<Pet> pets;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
@@ -11,6 +40,4 @@ public class Owner extends Person {
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
-
-    private Set<Pet> pets;
 }
