4106f6000036e161fe7b6cbfd42669876a37662b
Jorge <dam0ne@hemwick>
2020-06-03 16:51:04 +0200

    added speciality entity and added relationship to vet

---
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.springtraining.petclinic.model;
+
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Vet.java
@@ -1,4 +1,16 @@
 package com.springtraining.petclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
+
+    private Set<Speciality> specialities;
 }
