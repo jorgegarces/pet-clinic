e3064a8cc8727ec0a089965402af71865bbf1773
Jorge <dam0ne@hemwick>
2020-06-03 16:47:58 +0200

    added visits, updated owner with pet relationship

--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Owner.java
@@ -1,4 +1,16 @@
 package com.springtraining.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
+
+    private Set<Pet> pets;
 }
