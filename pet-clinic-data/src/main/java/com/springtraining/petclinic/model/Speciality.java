210af177203e9d5d3b44c58fc65579128d8b5552
Jorge <dam0ne@hemwick>
2020-06-15 12:15:18 +0200

    vet mapped as a jpa entity, speciality likeso

--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package com.springtraining.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package com.springtraining.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
