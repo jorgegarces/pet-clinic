5b301b5e00fba6bb9993fb3b707a8f155853c16d
Jorge <dam0ne@hemwick>
2020-06-15 12:03:46 +0200

    owner, pet and pettype converted as jpa entities, person set as mappedsuperclass

--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/PetType.java
@@ -1,7 +1,13 @@
 package com.springtraining.petclinic.model;
 
-public class PetType extends BaseEntity {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "types")
+public class PetType extends BaseEntity {
+    @Column(name = "name")
     private String name;
 
     public String getName() {
