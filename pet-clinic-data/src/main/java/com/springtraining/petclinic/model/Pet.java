0f79791dbfbc59c67f2adc631e45f46ec0618d2b
Jorge <dam0ne@hemwick>
2020-06-15 12:22:03 +0200

    added visit jpa mapping and modified pet mappings accordingly

--- pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.springtraining.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -51,5 +56,12 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
