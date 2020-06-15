03742d9bae5e2dda7a1c36ff2ec56628c383e024
Jorge <dam0ne@hemwick>
2020-06-15 17:33:25 +0200

    implement map impl for visit service

---
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springtraining.petclinic.services;
+
+import com.springtraining.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springtraining/petclinic/services/map/VisitMapService.java
@@ -0,0 +1,41 @@
+package com.springtraining.petclinic.services.map;
+
+import com.springtraining.petclinic.model.Visit;
+import com.springtraining.petclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null) {
+            throw new RuntimeException("Invalid visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        super.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+
+}
