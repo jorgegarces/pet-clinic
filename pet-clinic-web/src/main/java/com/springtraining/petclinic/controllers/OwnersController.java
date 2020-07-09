36a73847b975591453178272664ec51e6f11074e
Jorge <dam0ne@hemwick>
2020-07-09 10:01:12 +0200

    owner detail by id controller mapping

--- pet-clinic-web/src/main/java/com/springtraining/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/springtraining/petclinic/controllers/OwnersController.java
@@ -3,8 +3,12 @@ package com.springtraining.petclinic.controllers;
 import com.springtraining.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@RequestMapping("/owners")
 @Controller
 public class OwnersController {
 
@@ -14,7 +18,7 @@ public class OwnersController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners", "/owners/index.html"})
+    @RequestMapping({"/", "/index.html"})
     public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
@@ -22,8 +26,15 @@ public class OwnersController {
         return "/owners/index";
     }
 
-    @RequestMapping({"/owners/find", "/oups"})
+    @RequestMapping({"/find", "/oups"})
     public String findOwners() {
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 }
--- pet-clinic-web/src/test/java/com/springtraining/petclinic/controllers/OwnersControllerTest.java
+++ pet-clinic-web/src/test/java/com/springtraining/petclinic/controllers/OwnersControllerTest.java
@@ -2,23 +2,24 @@ package com.springtraining.petclinic.controllers;
 
 import com.springtraining.petclinic.model.Owner;
 import com.springtraining.petclinic.services.OwnerService;
-import org.aspectj.weaver.IHasSourceLocation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -46,17 +47,6 @@ class OwnersControllerTest {
     }
 
     @Test
-    void listOwners() throws Exception {
-
-        when(ownerService.findAll()).thenReturn(owners);
-
-        mockMvc.perform(get("/owners"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("/owners/index"))
-                .andExpect(model().attribute("owners", hasSize(2)));
-    }
-
-    @Test
     void listOwnersByIndex() throws Exception {
 
         when(ownerService.findAll()).thenReturn(owners);
@@ -75,5 +65,15 @@ class OwnersControllerTest {
                 .andExpect(view().name("notimplemented"));
 
         verifyNoInteractions(ownerService);
+    }
+
+    @Test
+    void displayOwner() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1l).build());
+
+        mockMvc.perform(get("/owners/123"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1l))));
     }
 }
